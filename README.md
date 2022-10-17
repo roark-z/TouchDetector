6ad5476e421477d18e2d640fee81a474a58883a4
Roark <roarkzhang42@gmail.com>
2022-10-17 16:05:42 -0700

    Update to circuitpython mpr121

--- README.md
+++ README.md
@@ -2,10 +2,12 @@
 Python code to use the MPR121 capacitive touch sensor from Adafruit with a Raspberry Pi to count/log touches, while ignoring 'un-touch' events.
 Counting/Logging times of touches happens in a threaded callback (using RPi.GPIO.add_event_callback) triggered from the IRQ pin on the MPR121. A custom callback can be installed which is run from the main callback. 
 
-Adafruit_MPR121 requires the Adafuit MPR121 library which, in turn, requires the Adafuit GPIO library for I2C-bus access:
-git clone https://github.com/adafruit/Adafruit_Python_MPR121 
-git clone https://github.com/adafruit/Adafruit_Python_GPIO
+Adafruit_MPR121 requires the Adafuit MPR121 library which, in turn, requires CircuitPython and Adafruit Blinka for I2C-bus access:
 
-NOTE :
-Adafruit_Python_MPR121 has been deprecated. Adafruit has a new module for mpr121 using CircuitPython at https://github.com/adafruit/Adafruit_CircuitPython_MPR121
-but this requires the whole CircuitPython install, which is rather large. It may be worth switching to this in the future.
+sudo pip3 install --upgrade adafruit-python-shell
+wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
+sudo python3 raspi-blinka.py
+pip3 install adafruit-circuitpython-busdevice
+pip3 install adafruit-circuitpython-mpr121
+
+Updated 2022/10/17 to use new CircutPython-based adafruit libraries.
--- TouchDetectorMPR121.py
+++ TouchDetectorMPR121.py
@@ -3,18 +3,12 @@
 
 """
 Python code to use the MPR121 capacitive touch sensor from Adafruit with a Raspberry Pi to count/log touches, while ignoring 'un-touch' events.
-Counting/Logging happens in a threaded callback (using RPi.GPIO.add_event_callback) triggered from the IRQ pin on the MPR121
+Counting/Logging happens in a threaded callback (using RPi.GPIO.add_event_callback) triggered from the IRQ pin on the MPR121.
 
-Adafruit_MPR121 requires the Adafuit MPR121 library which, in turn, requires the Adafuit GPIO library for I2C-bus access:
-git clone https://github.com/adafruit/Adafruit_Python_MPR121 
-git clone https://github.com/adafruit/Adafruit_Python_GPIO
-
-NOTE :
-Adafruit_Python_MPR121 has been deprecated. Adafruit has a new module for mpr121 using CircuitPython at github.com/adafruit/Adafruit_CircuitPython_MPR121
-but this requires the whole CircuitPython install, which is rather large. It may be worth switching to this in the future.
+See README for details.
 """
 
-from Adafruit_MPR121 import MPR121
+from adafruit_mpr121 import MPR121
 import RPi.GPIO as GPIO
 
 from array import array
@@ -23,7 +17,7 @@ from time import time, sleep
 gTouchDetector = None        # global reference to touchDetector for use in touchDetector callback
 
 
-class TouchDetector (MPR121.MPR121):
+class TouchDetector (MPR121):
     """
     *******************  TouchDetector inherits from Adafruit's MPR121 capacitive touch sensor code *************************
 
